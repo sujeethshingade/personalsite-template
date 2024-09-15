7e3ea5e1d8b4a7dec798276af14a095a61762e9a
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-09-15 08:07:10 +0530

    add header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,12 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <div className="flex justify-center items-center relative top-3">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        <a className="nav-item" href="#">Home</a>
+        <a className="nav-item" href="#">Projects</a>
+        <a className="nav-item" href="#">About</a>
+        <a className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" href="#">Contact</a>
+      </nav>
+    </div>
+  );
 };
