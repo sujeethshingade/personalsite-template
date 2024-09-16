a3b543cefc7f53db53a81d5dcbc51a50f8cb8ce4
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-09-16 13:03:41 +0530

    add tape section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
+import { TapeSection } from "@/sections/Tape";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <HeroSection />
+      <TapeSection />
     </div>
   );
 }
