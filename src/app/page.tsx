a2d7ed2808eda8e481990a8206a7afde203d1a8b
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-10-05 02:14:05 +0530

    add testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
+import { TestimonialsSection } from "@/sections/Testimonials";
 import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <HeroSection />
       <ProjectsSection />
       <TapeSection />
+      <TestimonialsSection />
       <ContactSection />
       <Footer />
     </div>
