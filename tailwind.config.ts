369323021eca86a9fb7bd569f0ac20b0851b14d1
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-10-05 02:51:09 +0530

    animate tape section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,12 +21,32 @@ const config: Config = {
       },
     },
     extend: {
-      fontFamily:{
+      fontFamily: {
         sans: 'var(--font-sans)',
         serif: 'var(--font-serif)',
-      }
+      },
+      animation: {
+        'move-left': 'move-left 1s linear infinite',
+      },
+      keyframes: {
+        "ping-large": {
+          "75%, 100%": {
+            transform: "scale(3)",
+            opacity: "0",
+          },
+        },
+        'move-left': {
+          '0%': {
+            transform: 'translateX(0%)',
+          },
+          '100%': {
+            transform: 'translateX(-50%)',
+          },
+        },
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
